6cda527d44fc6da7d84763e57b4910bdb3d1b7e4
Dave Mahoney <djmahoney.knights@gmail.com>
2021-02-24 13:49:08 -0500

    Work in Progress A2C

--- agents/A2C/A2C.py
+++ agents/A2C/A2C.py
@@ -36,7 +36,19 @@ class A2C:
         self.model = ActorCritic(state_dim, action_dim, n_latent_var).to(device)
         self.optimizer = optim.Adam(model.parameters())
 
+    def optimize_model(self):
+        dist, value = model()
         
+####################
+#   Memory Class   #
+####################
+
+class Memory:
+    def __init__(self):
+        self.actions = []
+        self.states = []
+        self.logprobs = []
+        self.rewards = []
 
 #########################
 #   Actor Critic Class   #
@@ -70,40 +82,3 @@ class ActorCritic(nn.Module):
         probs = self.critic(x)
         dist  = Categorical(probs)
         return dist, value
-
-    def act(self, state, memory):
-        state = torch.from_numpy(state).float().to(device)
-        action_probs = self.action_layer(state)
-
-        # Uses Boltzmann style exploration by sampling from distribution
-        dist = Categorical(action_probs)
-
-        # Multinomial uses the same distribution as Categorical but allows for sampling without replacement
-        # Enables us to grab non-duplicate actions faster
-        action_indices = torch.multinomial(action_probs,7,replacement=False)
-
-        # Append each action along with its log_prob and the current state separately
-        # Makes the loss function more manageable
-        for i in range(7):
-            memory.logprobs.append(dist.log_prob(action_indices[i]))
-            memory.states.append(state)
-            memory.actions.append(action_indices[i])
-
-        return action_indices
-
-    def evaluate(self, state, action):
-        action_probs = self.action_layer(state)
-
-        # Use same distribution as act
-        dist = Categorical(action_probs)
-
-        # Calculate the expected log_probs for the previous actions
-        action_logprobs = dist.log_prob(action)
-
-        # Calculate the entropy from the distribution
-        dist_entropy = dist.entropy()
-
-        # Get expected network output
-        state_value = self.value_layer(state)
-
-        return action_logprobs, torch.squeeze(state_value), dist_entropy
