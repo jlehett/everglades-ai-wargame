f567ce8412a8c1974f240075ec05ac3f1e80ca63
djmahoney <djmahoney.knights@gmail.com>
2021-03-07 12:25:53 -0500

    broken but working a2c

--- agents/A2C/A2C.py
+++ agents/A2C/A2C.py
@@ -10,7 +10,7 @@ import torch.optim as optim
 import torch.nn.functional as F
 from torch.distributions import Categorical
 
-#device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 # Hyperparameters
 learning_rate = 1e-2
@@ -20,14 +20,14 @@ epsilon = 0.99
 SavedAction = namedtuple('SavedAction', ['log_prob','value'])
 
 class A2C():
-    def __init__(self,action_space,observation_space, player_num, K_epochs):
+    def __init__(self,action_space,observation_space, K_epochs):
         self.state_space = observation_space.shape[0]
         self.action_space = action_space
-        self.K_epochs = K_epochs
+        self.K_epochs = 8
 
         self.memory = Memory()
         
-        self.shape = (self.action_space, 2)
+        self.shape = (7, 2)
 
         self.model = ActorCritic(self.state_space, self.action_space)
         self.optimizer = optim.Adam(self.model.parameters())
@@ -35,7 +35,7 @@ class A2C():
     def get_action(self, obs):
         action = np.zeros(self.shape)
         chosen_indices = self.model.act(obs, self.memory)
-
+  
         # Unwravel action indices to output to the env
         chosen_units = chosen_indices // 12
         chosen_nodes = chosen_indices % 11
@@ -44,37 +44,46 @@ class A2C():
         action[:,1] = chosen_nodes
 
         #log_prob = self.model.evaluate(obs, action)
+        print(action)
         return action
 
+    #def calc_return()
+
     def optimize_model(self):
-        R = 0
-        save_actions = self.model.save_actions
-        policy_loss = []
-        value_loss = []
+        # Monte Carlo estimate of state rewards:
         rewards = []
-        
-        for r in self.model.rewards[::-1]:
-            R = r + gamma * R
-            rewrds.insert(0,R)
-        
-        rewards = torch.tensor(rewards)
-        rewards = (rewards - rewards.mean()) / (rewards.std() + epsilon)
+        discounted_reward = 0
+        entropy = 0
+      
+        # Calculate reward discounts 
+        for reward in reversed(self.memory.rewards):
+            discounted_reward = reward + (gamma * discounted_reward)
+            rewards.insert(0, discounted_reward)
+       
+        # Normalizing the rewards:
+        rewards = torch.tensor(rewards, dtype=torch.float32).to(device)
+        rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-5)
 
-        for (log_prob, value), r in zip(save_actions, rewards):
+        # convert list to tensor
+        states = torch.stack(self.memory.states).to(device).detach()
+        actions = torch.stack(self.memory.actions).to(device).detach()
+        logprobs = torch.stack(self.memory.logprobs).to(device).detach()
+        
+        for _ in range(self.K_epochs):
+            logprobs, state_values, dist_entropy = self.model.evaluate(states, actions)
+            entropy += dist_entropy.mean()
             
-            reward = r - value.item()
-            policy_loss.append(-log_prob * reward)
-            value_loss.append(F.smooth_l1_loss(value, torch.tensor([r])))
+        advantage = rewards - state_values
 
-        print('policy loss', policy_loss)
-        print('value loss', value_loss)
+        actor_loss = -(logprobs * advantage.detach()).mean()
+        critic_loss = advantage.pow(2).mean()
+        loss = actor_loss + 0.5 * critic_loss - 0.001 * entropy
+
         self.optimizer.zero_grad()
-        loss = torch.stack(policy_loss).sum() + torch.stack(value_loss).sum()
         loss.backward()
         self.optimizer.step()
 
-        del self.model.rewards[:]
-        del self.model.save_actions[:]
+        self.memory.clear_memory()
 
 ####################
 #   Memory Class   #
@@ -101,8 +110,6 @@ class Memory:
 class ActorCritic(nn.Module):
     def __init__(self, state_dim, action_dim):
         super(ActorCritic, self).__init__()
-        # First layer
-        self.fc1 = nn.Linear(state_dim, 528)
 
         # actor
         self.actor = nn.Sequential(
@@ -114,21 +121,17 @@ class ActorCritic(nn.Module):
         # critic, return a scalar value
         self.critic = nn.Sequential(
             nn.Linear(state_dim, 528),
-            nn.Linear(528, action_dim)
+            nn.Linear(528, 1)
         )
 
-        self.save_actions = []
-        self.rewards = []
 
     def forward(self, x):
-        #x = F.relu(self.fc1(x))
-        action_score = self.actor(x)
-        state_value = self.critic(x)
-        return action_score, state_value
+        raise NotImplementedError
 
     def act(self, state, memory):
         state = torch.from_numpy(state).float()
         action_probs = self.actor(state)
+
         # Uses Boltzmann style exploration by sampling from distribution
         dist = Categorical(action_probs)
         
