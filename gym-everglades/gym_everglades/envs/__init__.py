4c4730fa4f0310c899a13d5575efc8e2cb4f4574
djmahoney <djmahoney.knights@gmail.com>
2021-04-03 23:28:01 -0400

    newton updates

--- gym-everglades/gym_everglades/envs/__init__.py
+++ gym-everglades/gym_everglades/envs/__init__.py
@@ -1,2 +1,5 @@
 from gym_everglades.envs.everglades_env import EvergladesEnv
-from gym_everglades.envs.everglades_renderer import EvergladesRenderer
+try:
+    from gym_everglades.envs.everglades_renderer import EvergladesRenderer
+except:
+    pass
